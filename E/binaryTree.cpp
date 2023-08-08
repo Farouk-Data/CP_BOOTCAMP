e4fd6f247a56aa1f8aec2f10d3e16b8bef2ce3dd
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-08 15:41:39 +0100

    thank god for vectors

--- E/binaryTree.cpp
+++ E/binaryTree.cpp
@@ -3,10 +3,12 @@
 #include <stdio.h>
 #include <set>
 #include <sstream>
+#include <vector>
 
 int main(){
     std::string line;
     std::set<std::string> st;
+    std::vector<std::string> vec;
 
     while (std::getline(std::cin, line)){
         std::istringstream iss(line);
@@ -17,12 +19,14 @@ int main(){
             st.insert(num);
         else if (cmd == "exists"){
             if (st.find(num) != st.end())
-                printf("true\n");
+                vec.push_back("true");
             else
-                printf("false\n");
+                vec.push_back("false");
         }
         else if (cmd == "delete")
             st.erase(num);
     }
+    for (const std::string& str : vec)
+        printf("%s\n", str.c_str());
     return (0);
 }
