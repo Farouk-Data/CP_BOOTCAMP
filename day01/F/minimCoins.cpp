da584975f4cd2b27951eb3b9b22d661104ebfc02
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-12 18:25:29 +0100

    coin problem solved

--- day01/F/minimCoins.cpp
+++ day01/F/minimCoins.cpp
@@ -3,13 +3,19 @@
 
 using namespace std;
 
-long long sol(long long x, vector<long long> &vec)
+long long sol(long long x, const vector<long long> &vec, vector<bool> &ready, vector<long long> &values)
 {
-    long long count = INT_MAX;
+    if (x < 0)
+        return (1e9);
     if (x == 0)
         return (0);
-    for (long long i : vec)
-        count = min(count, sol(x - i, vec) + 1);
+    if (ready[x])
+        return (values[x]);
+    long long count = 1e9;
+    for (long long i = 0; i < vec.size(); i++)
+        count = min(count , sol(x - vec[i], vec, ready, values) + 1);
+    values[x] = count;
+    ready[x] = true;
     return (count);
 }
 
@@ -17,11 +23,16 @@ int main (){
     long long n, x, tmp;
     vector<long long> vec;
     cin >> n >> x;
-
+    vector<bool> ready(x + 1);
+    vector<long long> values(x + 1);
     while (n--){
         cin >> tmp;
         vec.push_back(tmp);
     }
-    cout << sol(x, vec) << "\n";
+    long long res = sol(x, vec, ready, values);
+    if (res == 1e9)
+        cout << "-1" << "\n";
+    else
+        cout << res << "\n";
     return (0);
 }
