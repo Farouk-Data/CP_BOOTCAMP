5901fa32c509770504bb5bfff5bda4ed141c1747
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-14 11:08:28 +0100

    expo using binary search using iteration (faster than recursion)

--- day02/B/expo.cpp
+++ day02/B/expo.cpp
@@ -5,13 +5,25 @@
 using namespace std;
 
 LL expo(LL a, LL b){
-    LL res = 1, tmp = 1;
-    if (b == 0)
-        return (1);
-    tmp = expo(a % MOD, ((b % MOD) / 2) % MOD);
-    res = ((tmp % MOD) * (tmp % MOD)) % MOD;
-    if (b % 2 == 1)
-        res = ((a % MOD) * (res % MOD)) % MOD;
+    //using recursion
+
+    // LL res = 1, tmp = 1;
+    // if (b == 0)
+    //     return (1);
+    // tmp = expo(a % MOD, ((b % MOD) / 2) % MOD);
+    // res = ((tmp % MOD) * (tmp % MOD)) % MOD;
+    // if (b % 2 == 1)
+    //     res = ((a % MOD) * (res % MOD)) % MOD;
+    // return (res);
+
+    //using iteration
+    LL res = 1;
+    while (b > 0){
+        if (b % 2 == 1)
+            res = ((a % MOD) * (res % MOD)) % MOD;
+        a = (a % MOD) * (a % MOD);
+        b = ((b % MOD) / 2);
+    }
     return (res);
 }
 
