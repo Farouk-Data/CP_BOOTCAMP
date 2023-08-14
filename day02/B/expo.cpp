08b5ebc6964a64fe8f7a3c2a3445482360563c3c
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-14 10:54:55 +0100

    expo using binary search result by MOD

--- day02/B/expo.cpp
+++ day02/B/expo.cpp
@@ -8,10 +8,10 @@ LL expo(LL a, LL b){
     LL res = 1, tmp = 1;
     if (b == 0)
         return (1);
-    tmp = expo(a, b / 2);
-    res = tmp * tmp;
+    tmp = expo(a % MOD, ((b % MOD) / 2) % MOD);
+    res = ((tmp % MOD) * (tmp % MOD)) % MOD;
     if (b % 2 == 1)
-        res = a * res;
+        res = ((a % MOD) * (res % MOD)) % MOD;
     return (res);
 }
 
