c48c8254ba78d1d2fe1abc7c18e059a542408448
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-17 11:41:01 +0100

    solving equations using binary search

--- day02/E/sinEqua.cpp
+++ day02/E/sinEqua.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <math.h>
-#define LL long long 
+#include <bits/stdc++.h>
+#define LL long long
+#define LD long double
+#define EPS 1e-9 
 using namespace std;
 
-int main(){
-    LL in, a, b, c;
-    while (in--){
+//root finding problems
 
+LD equation(LD a, LD b, LD c, LD x){
+    return (a*x + b*sin(x) - c);
+}
+
+int main(){
+    LL in;
+    LD a, b, c;
+    cin >> in;
+    while (in--){
+        cin >> a >> b >> c;
+        LD low = 0, high = c + 100, mid;
+        while(high - low > EPS) {
+            mid = (low + high)/2;
+            if (equation(a,b,c,mid) > EPS)
+                high = mid;
+            else
+                low = mid;
+        }
+        cout << mid << endl;
     }
+    return (0);
 }
