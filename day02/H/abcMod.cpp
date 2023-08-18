11b8a8224fd3f85d8b10d9208e0aa53cb19d7ada
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-18 12:42:16 +0100

    multi and mod => adding context to the reminder calculus

--- day02/H/abcMod.cpp
+++ day02/H/abcMod.cpp
@@ -15,6 +15,8 @@ int main(){
     }
     abc = (((vec[0] * vec[1]) % MOD) * vec[2]) % MOD;
     def = (((vec[3] * vec[4]) % MOD) * vec[5]) % MOD;
+    //This addition of MOD ensures that the result will be a positive integer, even if abc - def is negative
+    //The entire result of (abc - def + MOD) is then taken modulo MOD. This step ensures that the final result remains within the range of 0 to MOD - 1, effectively calculating the remainder
     diff = (abc - def) % MOD;
     cout << diff << "\n";
     return (0);
