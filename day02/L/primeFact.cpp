d3f314e053e3a2685d024956bba962da6d55f1a3
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-19 22:50:43 +0100

    prime fact

---
+++ day02/L/primeFact.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#define LL long long
+using namespace std;
+ 
+void factorize(LL n, vector<LL> &vec)
+{
+    while (n % 2 == 0)
+    {
+        vec.push_back(2);
+        n = n/2;
+    }
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        while (n % i == 0)
+        {
+            vec.push_back(i);
+            n = n/i;
+        }
+    }
+    if (n > 2)
+        vec.push_back(n);
+}
+
+int main(){
+    LL in;
+    cin >> in;
+    vector<LL> vec;
+    factorize(in, vec);
+    cout << in << ": " ;
+    for(LL i : vec)
+        cout << i << " ";
+    return (0);
+}
